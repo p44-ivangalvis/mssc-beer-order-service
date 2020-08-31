4f8943d0a700ac7d14c691fc0b48e14a20a7494c
David Galvis <ivan.galvis@project44.com>
2020-08-31 08:16:16 -0500

    Adding the server port and the component annotation, to detect bean, closes #1

--- src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
@@ -20,7 +20,10 @@ package guru.sfg.beer.order.service.web.mappers;
 import guru.sfg.beer.order.service.domain.BeerOrder;
 import guru.sfg.beer.order.service.web.model.BeerOrderDto;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+
+@Component
 @Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
 public interface BeerOrderMapper {
 
