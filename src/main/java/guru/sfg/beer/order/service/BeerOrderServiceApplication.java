d917e63acc64429957ece9d676c5782ca371f31a
David Galvis <ivan.galvis@project44.com>
2020-09-07 17:01:09 -0500

    creating the jms config class, and the artemis user/password , closes #7

--- src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
+++ src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
@@ -2,8 +2,11 @@ package guru.sfg.beer.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+
+//this is a thing coming from JMS in the parent pom so this is generating some issues in the running of the service
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class )
 public class BeerOrderServiceApplication {
 
     public static void main(String[] args) {
