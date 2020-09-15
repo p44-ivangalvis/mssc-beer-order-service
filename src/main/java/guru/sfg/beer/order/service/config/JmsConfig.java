caa33ae3036f29ffb9ba485f6d9de37ae0b2af57
David Galvis <ivan.galvis@project44.com>
2020-09-15 09:38:23 -0500

    Refactoring the jms config so that it won't fail in the deserialization of objects, closes #14

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package guru.sfg.beer.order.service.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -12,12 +13,14 @@ import org.springframework.jms.support.converter.MessageType;
 public class JmsConfig
 {
     @Bean // Serialize message content to json using TextMessage
-    //public MessageConverter jacksonJmsMessageConverter( ObjectMapper objectMapper) {
-    public MessageConverter jacksonJmsMessageConverter( ) {
+    //if we dont specify an object mapper to work with, this jackson converter will never use it, so it will fail to convert objects or deserialize them
+    //this was the error Cannot construct instance of `java.time.OffsetDateTime` (no Creators, like default construct, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
+    public MessageConverter jacksonJmsMessageConverter( ObjectMapper objectMapper) {
+    //public MessageConverter jacksonJmsMessageConverter( ) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType( MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
-        //converter.setObjectMapper(objectMapper);
+        converter.setObjectMapper(objectMapper);
         return converter;
     }
 }
